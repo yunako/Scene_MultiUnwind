edef2e4f65236737441e49069f6890d25ca46201
yunako <go2837@hanmail.net>
2019-06-29 17:47:27 +0900

    wind/unwind test

---
+++ Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by YUNA KO on 29/06/2019.
+//  Copyright © 2019 YUNAKO. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController : UIViewController {
+    @IBAction func gotoPage2(_ sender: UIStoryboardSegue){
+        
+    }
+}
---
+++ Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by YUNA KO on 29/06/2019.
+//  Copyright © 2019 YUNAKO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue){
+        
+    }
+
+}
+
